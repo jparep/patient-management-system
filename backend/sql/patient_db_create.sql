27a87d53d1d418f46a6624acc52d2411c3adda57
jparep <parepson@gmail.com>
2024-12-11 00:14:22 -0700

    Usersa nd Patatients table created

--- backend/sql/patient_db_create.sql
+++ backend/sql/patient_db_create.sql
@@ -8,3 +8,17 @@ CREATE TABLE users (
     role ENUM('admin', 'doctor', 'receptionist', 'accountant') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+# Stores details about patients.
+CREATE TABLE patients (
+    patient_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender ENUM('male', 'female', 'other') NOT NULL,
+    phone_number VARCHAR(15) UNIQUE NOT NULL,
+    email VARCHAR(255),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
