a11b8cd3d2bb36b9740df7ff0906b99be4168864
jparep <parepson@gmail.com>
2024-12-14 01:14:19 -0700

    Terraform provider file created

---
+++ deployment/terraform/provider.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = module.eks.cluster_auth_token
+}
